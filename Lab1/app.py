b19341f461ef6f4bc7aa8231808df8cf7f5e2e26
Maciej Szulc <mszulc@vp.pl>
2019-12-16 15:52:25 +0100

    Python 3

--- Lab1/app.py
+++ Lab1/app.py
@@ -2,27 +2,27 @@ from flask import Flask
 app = Flask(__name__)
 
 version = "1.0"
-print "App ready."
+print ("App {} ready.".format(version))
 
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+    return ("Hello World!")
 
 
 @app.route('/alive')
 def alive():
-    return "Yes, I'm v.{} and alive.".format(version)
+    return ("Yes, I'm v.{} and alive.".format(version))
 
 
 @app.route('/version')
 def show_version():
-    return "Ver: %s" % (version)
+    return ("Ver: %s" % (version))
 
 
 @app.route('/<echo>')
 def send_echo(echo):
-    return "{}".format(echo)
+    return ("{}".format(echo))
 
 
 if __name__ == '__main__':
